1b9de0ca3b9d82ece4262c5f7873fa5b2f49f8c2
Adriel Izoton de Faci <adriel.faci@promob.com>
2021-03-11 11:24:56 -0300

    better look to the page

--- js/controller.js
+++ js/controller.js
@@ -2,6 +2,30 @@ function test() {
   alert("foi");
 }
 
-$(window).on("load", function() {
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+$(window).on("load", function () {
+  // $(".bg").attr("background-image", 'url("../wallpapers/wallpaper0'+ this.getRndInteger(0,5) + '.jpg");');
   $("#loaderWrapper").hide();
+});
+$(document).ready(function () {
+  var images = ['./wallpapers/wallpaper00.jpg',
+    './wallpapers/wallpaper01.jpg',
+    './wallpapers/wallpaper02.jpg',
+    './wallpapers/wallpaper03.jpg',
+    './wallpapers/wallpaper04.jpg'];
+
+  var randomNumber = Math.floor(Math.random() * images.length);
+  var bgImg = 'url(' + images[randomNumber] + ')';
+
+  $('body').css({
+    'background': bgImg,
+    'height': '100%',
+    'background-position': 'center',
+    'background-repeat': 'no-repeat',
+    'background-size': 'cover'
+  });
+
 });
