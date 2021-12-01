ac88241e5a8107cfbcbef0e1b913e072d33bcc9f
Adriel Izoton de Faci <adriel.idf@gmail.com>
2021-11-30 23:35:51 -0300

    new look for the website

--- js/controller.js
+++ js/controller.js
@@ -6,11 +6,7 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-$(window).on("load", function () {
-  // $(".bg").attr("background-image", 'url("../wallpapers/wallpaper0'+ this.getRndInteger(0,5) + '.jpg");');
-  $("#loaderWrapper").hide();
-});
-$(document).ready(function () {
+function setWallpaper(lastNumber) {
   var images = ['./wallpapers/wallpaper00.jpg',
     './wallpapers/wallpaper01.jpg',
     './wallpapers/wallpaper02.jpg',
@@ -25,7 +21,16 @@ $(document).ready(function () {
     'height': '100%',
     'background-position': 'center',
     'background-repeat': 'no-repeat',
-    'background-size': 'cover'
+    'background-size': 'cover',
+    '-webkit-transition': 'background-image 0.6s ease-in-out',
+    'transition': 'background-image 0.6s ease-in-out'
   });
+  setTimeout(setWallpaper, 6000);
+}
 
+$(window).on("load", function () {
+  $("#loaderWrapper").hide();
+});
+$(document).ready(function () {
+  setWallpaper();
 });
